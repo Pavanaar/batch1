9f7d0d87501e6e9bc51dad33a9c2a89f9575804d
pavanaar <pavanagowda2018@gmail.com>
2024-07-11 15:55:55 +0530

    new file aa

--- test/specs/rrr.js
+++
@@ -1,9 +0,0 @@
-import excel from "../genericUlility/excel.js";
-describe("read utility",async()=>{
-    it("readdata and write",async()=>{
-    
-       // await excel.readdata("./test/testdata/exceldata.xlsx","Sheet1",1,1)
-        console.log(await excel.readdata("./test/testdata/exceldata.xlsx","Sheet1",1,2));
-        await excel.writedataa("./test/testdata/exceldata.xlsx","Sheet4",1,2,"wdio")
-    })
-})
